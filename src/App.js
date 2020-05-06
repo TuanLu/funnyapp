33fc395a482178b6a6602170cac30c2944504115
Tim <tuan.mrbean@gmail.com>
2020-05-06 23:55:40 +0700

    Test deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-			<h1>Test Deploy</h1>
+			<h1 style={{color: 'red'}}>Test Deploy</h1>
 			<p>New Text</p>
     </div>
   );
