5554e1f325f63459e9fb34042a26f18244f1b84a
Tim <tuan.mrbean@gmail.com>
2020-05-07 10:50:00 +0700

    setup sass, logo, samle img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Logo from './assets/light-logo.png'
+import Avatar from './assets/avatar.jpg'
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{color: 'red'}}>Test Deploy - Sass</h1>
+    <div className="wrapper">
+			<div className="header">
+				<img className="header__logo" src={Logo} alt="logo" />
+				<span className="heading-title">Grow your idea here</span>
+			</div>
+			<div className="main">
+				<div className="main__sidebar">
+					Sidebar
+				</div>
+				<div className="main__content">
+					<img className="main__thumb" src={Avatar} alt="avatar" />
+				</div>
+			</div>
+			<div className="footer">
+				Footer
+			</div>
     </div>
   );
 }
